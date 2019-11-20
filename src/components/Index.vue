719b0302ecedaa87a783132abe44ad5dcb90717a
smallbo <smallbo0913@163.com>
2019-11-20 10:29:24 +0800

    重新建

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-
+    <h3>首页</h3>
   </div>
 </template>
 
 <script>
   export default {
-    
+
   }
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/views/Index'
+import axios from 'axios'
 Vue.use(Router)
-
+Vue.use(ElementUI);
+Vue.prototype.$http=axios;
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Index
     }
   ]
 })
